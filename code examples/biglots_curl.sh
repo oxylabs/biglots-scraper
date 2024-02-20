8316b4730102d097e66447210ccdb4b9426afb61
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:16:16 +0200

    Adding code examples/biglots_curl.sh

---
+++ code examples/biglots_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.biglots.com/c/furniture/office-furniture/_/n-916368633?scm=furniture_jas23_viznav_inspo_office"}'
